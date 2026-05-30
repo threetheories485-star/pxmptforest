09c6e58f3b075fabcefabbe7eb5352595234ad77
threetheories485-star <threetheories485@gmail.com>
2026-05-30 13:55:29 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+// sw.js - Blazing Fast Service Worker Cache
+const CACHE_NAME = 'pxmpt-forest-v6';
+const ASSETS = [
+  './',
+  './index.html',
+  './styless.css',
+  './app.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        event.waitUntil(
+          fetch(event.request).then((networkResponse) => {
+            if (networkResponse && networkResponse.status === 200) {
+              caches.open(CACHE_NAME).then((cache) => cache.put(event.request, networkResponse.clone()));
+            }
+          }).catch(() => {
+            // ignore network failures
+          })
+        );
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
